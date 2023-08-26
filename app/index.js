1d4ec7d648aba83241db11ff6454435d2d4a9dd0
Afeef <afeef9706@gmail.com>
2023-08-27 01:51:26 +0800

    Added Inconsolata font

--- app/index.js
+++ app/index.js
@@ -1,20 +1,38 @@
-import React from "react";
-import { Pressable } from "react-native";
+import React, { useCallback } from "react";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
 import Home from "../src/screens/Home";
-import { Stack } from "expo-router";
-import HamburgerMenu from "../src/components/shared/HamburgerMenu";
-import HeaderTitle from "../src/components/shared/HeaderTitle";
+import Header from "../src/components/Header";
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+    const [fontsLoaded] = useFonts({
+        ExtraLight: require("../src/assets/fonts/Inconsolata-ExtraLight.ttf"),
+        Light: require("../src/assets/fonts/Inconsolata-Light.ttf"),
+        Regular: require("../src/assets/fonts/Inconsolata-Regular.ttf"),
+        Medium: require("../src/assets/fonts/Inconsolata-Medium.ttf"),
+        SemiBold: require("../src/assets/fonts/Inconsolata-SemiBold.ttf"),
+        Bold: require("../src/assets/fonts/Inconsolata-Bold.ttf"),
+        Bolder: require("../src/assets/fonts/Inconsolata-ExtraBold.ttf"),
+        Black: require("../src/assets/fonts/Inconsolata-Black.ttf"),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        console.log(fontsLoaded);
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <>
-            <Stack.Screen
-                options={{
-                    headerTitle: () => <HeaderTitle />,
-                    headerRight: () => <HamburgerMenu />,
-                }}
-            />
-            <Home />
+            <Header />
+            <Home onLayout={onLayoutRootView} />
         </>
     );
 };
