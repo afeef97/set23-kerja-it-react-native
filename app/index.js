2cf64314f38f9ab53facb3bc6d1a2da8fafe4a56
Afeef Abu Bakar <46017353+afeef97@users.noreply.github.com>
2023-08-26 15:13:50 +0800

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+    return (
+        <View className="justify-center items-center min-h-screen">
+            <Text className="text-2xl">Hello World</Text>
+        </View>
+    );
+};
+
+export default App;
