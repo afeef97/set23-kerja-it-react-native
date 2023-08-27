e32c11e30ebaa2396dfe90d319b4f0e83bc1efb5
Afeef <afeef9706@gmail.com>
2023-08-27 19:52:34 +0800

    Changed card styling

--- src/components/shared/JobCard.jsx
+++ src/components/shared/JobCard.jsx
@@ -9,37 +9,49 @@ const JobCard = ({ title, company, posted, location, workType }) => {
             <TextInconsolata textStyle="text-lg" boldLevel="bold">
                 {title}
             </TextInconsolata>
-            <TextInconsolata textStyle="text-base">{company}</TextInconsolata>
+            <TextInconsolata textStyle="text-base text-gray-500">
+                {company}
+            </TextInconsolata>
 
             <View className="mt-2">
-                <View className="flex-row">
+                <View className="flex-row items-center">
                     <Octicons
                         name="calendar"
                         style={{ marginRight: 5 }}
                         size={16}
-                        color="black"
+                        color="gray"
                     />
-                    <TextInconsolata>Posted on {posted}</TextInconsolata>
+                    <TextInconsolata textStyle="text-base text-gray-500">
+                        Posted on {posted}
+                    </TextInconsolata>
                 </View>
-                <View className="flex-row">
+                <View className="flex-row items-center">
                     <Octicons
                         name="location"
                         style={{ marginRight: 5 }}
                         size={16}
-                        color="black"
+                        color="gray"
                     />
-                    <TextInconsolata>{location}</TextInconsolata>
+                    <TextInconsolata textStyle="text-base text-gray-500">
+                        {location}
+                    </TextInconsolata>
                 </View>
-                <View className="flex-row">
+                <View className="flex-row items-center">
                     <Octicons
                         name="briefcase"
                         style={{ marginRight: 5 }}
                         size={16}
-                        color="black"
+                        color="gray"
                     />
-                    <TextInconsolata>{workType}</TextInconsolata>
+                    <TextInconsolata textStyle="text-base text-gray-500">
+                        {workType}
+                    </TextInconsolata>
                 </View>
             </View>
+
+            <TextInconsolata textStyle="text-base text-gray-500">
+                Added on {posted}
+            </TextInconsolata>
         </View>
     );
 };
